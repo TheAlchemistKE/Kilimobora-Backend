16f624a7bdf0791e39585d455816705835823605
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-12-06 09:55:59 +0300

    Separation of concern in helper.py

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ This is an AI-assisted IoT Smart Farming application that has the following feat
 1. A farmer dashboard where they can monitor the soil moisture content, humidity, temperature and the plant health.
 2. A farmer can be able to view a summary of the plant variables.
 3. Farmers can be able to view a bar graph of all variables.
+
+### Getting Started
+### Installation
+### Demo
+### Author
+### Contributors
--- util/helper.py
+++ util/helper.py
@@ -17,31 +17,31 @@ client = MongoClient(mongodb)
 db = client['kilimobora']
 collection = db['sensor_data']
 
-upload_interval = 10
-while 1:
-    try:
-        moisture_reading = serial_conn.readline().rstrip()
-        temp_reading = serial_conn.readline().rstrip()
 
-        if moisture_reading:
-            doc_id = collection.insert_one({
+
+
+def fetch_sensor_data() -> dict:
+    moisture_reading = serial_conn.readline().rstrip()
+    temp_reading = serial_conn.readline().rstrip()
+    if moisture_reading and temp_reading:
+        return {
                 'moisture': moisture_reading,
                 'temperature': temp_reading,
                 'timestamp': datetime.datetime.now(),
                 'location': location
 
-            }).inserted_id
-    except serial.SerialTimeoutException:
-        print('Could not read data from unit')
-    finally:
-        time.sleep(upload_interval)
-
-
-
-
-def fetch_sensor_data() -> dict:
-    pass
-
+        }
+    else:
+        return {
+            'message': 'Sensors offline'
+        }
 def save_data():
-    pass
+    delay = 10
+    while 1:
+        try:
+            doc_id = collection.insert_one().inserted_id
+        except serial.SerialTimeoutException:
+            print('Could not read data from unit')
+        finally:
+            time.sleep(delay)
 
